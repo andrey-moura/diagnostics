62a5e9cfb8ca3e034e7de7e98da4d0d2953e4785
andrey moura <andreymds88@gmail.com>
2022-07-24 01:54:42 -0300

    first commit

---
+++ diagnostics.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <chrono>
+#include <functional>
+#include <type_traits>
+
+namespace uva
+{
+	namespace diagnostics
+	{
+		template<typename functor, typename... Args>
+		auto measure_function(const functor& function, Args&&... args) {
+			if constexpr (std::is_same<void, decltype(function(std::forward<Args>(args)...))>::value) {
+				auto start = std::chrono::high_resolution_clock::now();
+				function(std::forward<Args>(args)...);
+				auto end = std::chrono::high_resolution_clock::now();
+				return std::chrono::duration<double>(end - start);
+			}
+			else {
+				auto start = std::chrono::high_resolution_clock::now();
+				auto ret = function(std::forward<Args>(args)...);
+				auto end = std::chrono::high_resolution_clock::now();
+				return std::make_pair<decltype(ret), std::chrono::duration<double>>(std::move(ret), std::chrono::duration<double>(end - start));
+			}
+		}
+	};
+};
