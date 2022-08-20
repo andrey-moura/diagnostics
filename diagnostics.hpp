06e20668b7a46ae813ba2e070ae2eaba6c029b34
andrey moura <andreymds88@gmail.com>
2022-08-20 17:54:47 -0300

    By default, measure function now returns nanoseconds

--- diagnostics.hpp
+++ diagnostics.hpp
@@ -14,13 +14,13 @@ namespace uva
 				auto start = std::chrono::high_resolution_clock::now();
 				function(std::forward<Args>(args)...);
 				auto end = std::chrono::high_resolution_clock::now();
-				return std::chrono::duration<double>(end - start);
+				return std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
 			}
 			else {
 				auto start = std::chrono::high_resolution_clock::now();
 				auto ret = function(std::forward<Args>(args)...);
 				auto end = std::chrono::high_resolution_clock::now();
-				return std::make_pair<decltype(ret), std::chrono::duration<double>>(std::move(ret), std::chrono::duration<double>(end - start));
+				return std::make_pair<decltype(ret), std::chrono::duration_cast<std::chrono::nanoseconds>>(std::move(ret), std::chrono::duration_cast<std::chrono::nanoseconds>(end - start));
 			}
 		}
 	};
